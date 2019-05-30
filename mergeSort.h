f81adbff96168502694665189fd90da4efa2a4b9
JamesBustamante <42855903+JamesBustamante@users.noreply.github.com>
2019-05-30 20:34:34 +1000

    Update mergeSort.h

--- mergeSort.h
+++ mergeSort.h
@@ -1,18 +1,27 @@
+
+
 /* Determines whether the first node is ahead of the second, alphabetically by node title. */
 /* Returns 1 if 'node1' is first, -1 if 'node2' is first, 0 if nodes have the same title.  */
 int alphabeticalOrderTitle(struct node* node1, struct node* node2);
-int alphabeticalOrderUsername(struct node* node1, struct node* node2);
 
 /* Merge Sort main function, recursively calls itself to sort a provided linkedlist. */
 void mergeSortMainTitle(struct node** headReference);
-void mergeSortMainUsername(struct node** headReference);
 
 /* Merge Sort split function, finds the mid point of an array and splits it into two. */
 void mergeSortSplitTitle(struct node* list, struct node** frontPtr, struct node** backPtr);
-void mergeSortSplitUsername(struct node* list, struct node** frontPtr, struct node** backPtr);
 
 /* Merge Sort merge function, orders elements alphabetically and merges them. */
 struct node* mergeSortMergeTitle(struct node* node1, struct node* node2);
+
+int alphabeticalOrderUsername(struct node* node1, struct node* node2);
+
+/* Merge Sort main function, recursively calls itself to sort a provided linkedlist. */
+void mergeSortMainUsername(struct node** headReference);
+
+/* Merge Sort split function, finds the mid point of an array and splits it into two. */
+void mergeSortSplitUsername(struct node* list, struct node** frontPtr, struct node** backPtr);
+
+/* Merge Sort merge function, orders elements alphabetically and merges them. */
 struct node* mergeSortMergeUsername(struct node* node1, struct node* node2);
 
 int alphabetOrderTitle (struct node* a, struct node* b) {
@@ -116,23 +125,24 @@ struct node* mergeSortMergeTitle(struct node* a, struct node* b){
 	}
 
 	/* Get the alphabet order int. */
-	int alphabeticalOrderVal = alphabetlOrderTitle(a, b);
+	int alphabeticalOrderVal = alphabetOrderTitle(a, b);
 
 	/* If the first string comes first, add that. */
-	if(alphabeticalOrder == 1){
+	if(alphabeticalOrderVal == 1){
 		result = a;
 		result-> next = mergeSortMergeTitle(a->next, b);
 	}
-	else if (alphabeticalOrder == 2){
+	else if (alphabeticalOrderVal == 2){
 		result = b;
 		result -> next = mergeSortMergeTitle(a, b->next);
 	}
 	return result;
+
 }
 
 int alphabetOrderUsername (struct node* a, struct node* b) {
 	char* aString = a->username;
-	char* bString = b->uesrname;
+	char* bString = b->username;
 	int y = 0;
 	int j = 0;
 
@@ -231,14 +241,14 @@ struct node* mergeSortMergeUsername(struct node* a, struct node* b){
 	}
 
 	/* Get the alphabet order int. */
-	int alphabeticalOrderVal = alphabetlOrderUsername(a, b);
+	int alphabeticalOrderVal = alphabetOrderUsername(a, b);
 
 	/* If the first string comes first, add that. */
-	if(alphabeticalOrder == 1){
+	if(alphabeticalOrderVal == 1){
 		result = a;
 		result-> next = mergeSortMergeUsername(a->next, b);
 	}
-	else if (alphabeticalOrder == 2){
+	else if (alphabeticalOrderVal == 2){
 		result = b;
 		result -> next = mergeSortMergeUsername(a, b->next);
 	}
